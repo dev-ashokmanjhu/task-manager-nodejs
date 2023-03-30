64af96410786fba7fe7290696e65eb70bb5c4fbc
Ashok Manjhu <dev.ashokmanjhu@gmail.com>
2023-03-30 11:55:18 +0530

    make api for all routes

--- src/index.js
+++ src/index.js
@@ -10,53 +10,113 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Server is Running Successfully");
 });
-app.post("/users", (req, res) => {
-  const user = new User(req.body);
-  user
-    .save()
-    .then((user) => {
-      res.send(user);
-    })
-    .catch((err) => res.send(err));
+app.post("/users", async (req, res) => {
+  const user = await new User(req.body);
+  try {
+    user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(err);
+  }
 });
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => res.send(users))
-    .catch((e) => res.send(e));
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.send(error);
+  }
 });
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
   const _id = req.params.id;
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send("User Not Found");
-      }
-      res.send(user);
-    })
-    .catch((err) => res.send(err));
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send("User Not Found");
+    }
+    res.send(user);
+  } catch (error) {
+    res.send(error);
+  }
 });
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => res.send(tasks))
-    .catch((err) => res.send(err));
+app.put("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidator: true,
+    });
+    if (!user) {
+      res.status(404).send("User Not Found");
+    }
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(404).send(error);
+  }
 });
-app.get("/tasks/:id", (req, res) => {
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      res.status(404).send("User Not Found");
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (error) {
+    res.send(error);
+  }
+});
+app.get("/tasks/:id", async (req, res) => {
   const _id = req.params.id;
-  Task.findById(_id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send("Task Not Found");
-      }
-      res.send(task);
-    })
-    .catch((err) => res.send(err));
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send("Task Not Found");
+    }
+    res.send(task);
+  } catch (error) {
+    res.send(error);
+  }
 });
-app.post("/task", (req, res) => {
+app.put("/task/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidator: true,
+    });
+    if (!task) {
+      res.status(404).send("Task not Found");
+    }
+    res.status(200).send(task);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+app.delete("/task/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      res.status(404).send("Task not Found");
+    }
+    res.status(200).send(task);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+app.post("/task", async (req, res) => {
   const task = new Task(req.body);
-  task
-    .save()
-    .then((task) => res.send(task))
-    .catch((err) => res.send(err));
+  try {
+    await task.save();
+    res.send(task);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.listen(port, () => {
--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User", {
   name: {
     type: String,
+    required: true,
   },
   age: {
     type: Number,
