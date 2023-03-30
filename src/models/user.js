6dec7aa855d38192f1532a34feef57f4cdced6c1
Ashok Manjhu <dev.ashokmanjhu@gmail.com>
2023-03-30 12:13:47 +0530

    update

--- src/models/user.js
+++ src/models/user.js
@@ -21,44 +21,5 @@ const User = mongoose.model("User", {
     },
   },
 });
-// const me = new User({
-//   name: "Mike",
-//   age: 32,
-//   password: "4354233",
-// });
-
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch((error) => {
-//     console.log("Error!", error);
-//   });
 
 module.exports = User;
-
-// const Task = mongoose.model("Task", {
-//   description: {
-//     type: String,
-//     trim: true,
-//     required: true,
-//   },
-//   completed: {
-//     type: Boolean,
-//     default: false,
-//   },
-// });
-
-// const task = new Task({
-//   description: "Learn the Mongoose library",
-//   completed: false,
-// });
-
-// task
-//   .save()
-//   .then(() => {
-//     console.log(task);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
