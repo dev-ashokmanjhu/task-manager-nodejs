14dae35d6f81f6416d8eaa2c4a6532ffb75edaad
Ashok <ashokmanjhu133@gmail.com>
2023-08-26 13:56:36 +0530

    test

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require("./app");
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is Running🏃🏃 on Port : ${port}`);
+  console.log(`Server is Running🏃 on Port : ${port}`);
 });
